25f1328ca8e53effe54c99f4b00ff53b2f340d40
Rakshithsreddy123 <rakshithreddy318@gmail.com>
2026-07-19 17:53:28 +0530

    updated api.js file

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8001";
+const API_BASE = import.meta.env.VITE_API_URL ||"http://localhost:8001";
 
 export async function analyzeReportText(reportText) {
   const response = await fetch(`${API_BASE}/analyze`, {
